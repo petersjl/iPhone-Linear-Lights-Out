ba9302c1b9e4cac9d8aec92703bf14edf9b94ad3
espinood <espinood@rose-hulman.edu>
2020-04-13 18:33:00 -0400

    Initial Commit

---
+++ Lights Out/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lights Out
+//
+//  Created by CSSE Department on 4/13/20.
+//  Copyright © 2020 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
